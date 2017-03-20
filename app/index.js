289e21de52dd501e95698b0ac14aa567ab848d2c
Jimut Dhali <jimutdhali@LPMB-Kol-Jimut.local>
2017-03-20 19:11:07 +0530

    Refactoring

--- app/index.js
+++ app/index.js
@@ -12,9 +12,30 @@ const x = d3.scaleLinear().rangeRound([0, width]);
 const y = d3.scaleLinear().rangeRound([0, height]);
 const line = d3.line()
   .x(d => x(d.x))
-  .y(d => y(d.y))
-  .defined(true);
+  .y(d => y(d.y));
 
+// Make shadow filter
+// const defs = svg.append('defs');
+// const filter = defs.append('filter')
+//   .attr('id', 'drop-shadow')
+//   .attr('height', '130%');
+//
+// filter.append("feGaussianBlur")
+//   .attr("in", "SourceAlpha")
+//   .attr("stdDeviation", 5)
+//   .attr("result", "blur");
+//
+// filter.append("feOffset")
+//   .attr("in", "blur")
+//   .attr("dx", 5)
+//   .attr("dy", 5)
+//   .attr("result", "offsetBlur");
+//
+// const feMerge = filter.append("feMerge");
+// feMerge.append("feMergeNode")
+//   .attr("in", "offsetBlur");
+// feMerge.append("feMergeNode")
+//   .attr("in", "SourceGraphic");
 
 d3.json('app/data.json', (err, data) => {
   if (err)
@@ -26,7 +47,8 @@ d3.json('app/data.json', (err, data) => {
 
   // Now draw
   let tracks = g.selectAll('.tracks').data(data)
-    .enter().append('g')
+    .enter()
+    .append('g')
     .attr('class', 'tracks')
     .append('path')
     .attr('d', d => (line(d.coords)));
---
+++ readme.md
@@ -0,0 +1,12 @@
+Train Topology
+==============
+Train topology implementation
+
+Instructions
+-----------
+1. Clone the repo
+3. run `npm i -g webpack webpack-dev-server`
+2. run `npm install`
+3. run `npm run serve`
+
+To build run `npm run build`
