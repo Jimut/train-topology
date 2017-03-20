1845f5a88a6303e1ee411d9c31d6e73677b8664c
Jimut Dhali <jimutdhali@LPMB-Kol-Jimut.local>
2017-03-20 18:02:25 +0530

    Basic Structuring

--- app/index.js
+++ app/index.js
@@ -1,7 +1,33 @@
-import d3 from 'd3';
+import * as d3 from 'd3';
 
+// Initialize svg
 const svg = d3.select('svg');
-const margin = {top: 20, right: 20, bottom: 30, left: 50};
+const margin = {top: 20, right: 20, bottom: 20, left: 20};
 const width = +svg.attr('width') - margin.left - margin.right;
 const height = +svg.attr('height') - margin.top - margin.bottom;
 const g = svg.append('g').attr('transform', `translate(${margin.left},${margin.top})`);
+
+// Make ranges
+const x = d3.scaleLinear().rangeRound([0, width]);
+const y = d3.scaleLinear().rangeRound([0, height]);
+const line = d3.line()
+  .x(d => x(d.x))
+  .y(d => y(d.y))
+  .defined(true);
+
+
+d3.json('app/data.json', (err, data) => {
+  if (err)
+    console.warn(err);
+
+  // Set domains
+  x.domain(d3.extent([...data.map(d => d.coords[0].x), ...data.map(d => d.coords[1].x)]));
+  y.domain(d3.extent([...data.map(d => d.coords[0].y), ...data.map(d => d.coords[1].y)]));
+
+  // Now draw
+  let tracks = g.selectAll('.tracks').data(data)
+    .enter().append('g')
+    .attr('class', 'tracks')
+    .append('path')
+    .attr('d', d => (line(d.coords)));
+});
